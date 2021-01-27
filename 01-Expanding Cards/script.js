e3447986af026832ec9f5e1e99dbafe4d4fe3ecc
dwhenson <dave.henson@gmail.com>
2021-01-27 17:03:44 +0300

    ✅ DONE: 01 Expanding cards completed

--- 01-Expanding Cards/script.js
+++ 01-Expanding Cards/script.js
@@ -1,1 +1,29 @@
-console.log("it works");
+const images = [...document.querySelectorAll(".panel")];
+const gallery = document.querySelector(".container");
+
+function showHeaders(image) {
+	image.addEventListener(
+		"transitionend",
+		() => {
+			image.querySelector("h3").style.display = "block";
+		},
+		{ once: true }
+	);
+}
+
+function expandImage(event) {
+	if (!event.target.closest(".container")) return;
+	if (event.target.classList.contains("active")) return;
+	images.forEach((image) => {
+		image.classList.remove("active");
+		image.querySelector("h3").style.display = "none";
+		if (image === event.target) {
+			image.classList.add("active");
+		}
+	});
+	showHeaders(event.target);
+}
+
+gallery.addEventListener("click", function (event) {
+	expandImage(event);
+});
