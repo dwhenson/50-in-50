4a06842d9a62e92fc683e34e18ae6c758fde7a2c
dwhenson <dave.henson@gmail.com>
2021-01-30 06:24:09 +0300

    ✨IMPROVE: 02 Progress Steps - CSS transitions, JS structure and comments

--- 02-Progress Steps/script.js
+++ 02-Progress Steps/script.js
@@ -1,8 +1,32 @@
+/* =================== Variables ====================== */
+/* ==================================================== */
+
 const stages = [...document.querySelectorAll(".circle")];
 const next = document.querySelector("#next");
 const prev = document.querySelector("#prev");
 const progress = document.querySelector("#progress");
 let count = 0;
+
+/* =================== Functions ====================== */
+/* ==================================================== */
+
+/* Helper Functions
+/* ==================================================== */
+
+function colorCirclesUp() {
+	stages[count].classList.add("active");
+}
+
+function colorCirclesDown() {
+	stages[count].classList.remove("active");
+}
+
+function adjustProgress() {
+	progress.style.width = `${(count / (stages.length - 1)) * 100}%`;
+}
+
+/* App Functions
+/* ==================================================== */
 
 function checkButtonState() {
 	if (count === stages.length - 1) {
@@ -17,23 +41,32 @@ function checkButtonState() {
 	}
 }
 
-function colorCircles(event) {
+function increaseProgress(event) {
 	if (event.target === next && count < stages.length - 1) {
 		count += 1;
-		stages[count].classList.add("active");
+		document.addEventListener("transitionend", colorCirclesUp, {
+			once: true,
+		});
+		adjustProgress();
 	}
 	if (event.target === prev && count > 0) {
-		stages[count].classList.remove("active");
+		colorCirclesDown();
 		count -= 1;
+		adjustProgress();
 	}
 }
+
+/* Handler Functions
+/* ==================================================== */
 
 function clickHandler(event) {
 	event.preventDefault();
 	if (event.target.tagName !== "BUTTON") return;
-	colorCircles(event);
+	increaseProgress(event);
 	checkButtonState();
-	progress.style.width = `${count * 33.333}%`;
 }
+
+/* ============  Inits and Event Listeners  =========== */
+/* ==================================================== */
 
 document.addEventListener("click", clickHandler);
